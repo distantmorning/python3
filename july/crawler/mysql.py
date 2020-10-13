d043aa1bcc836273367027055fc920027eab01cb
lfelse <dingleiyt@gmail.com>
2020-10-13 17:26:57 +0800

    个

--- july/crawler/mysql.py
+++ july/crawler/mysql.py
@@ -3,7 +3,7 @@ import pymysql
 #操作mysql数据库
 #插入功能
 
-db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='spiders')
+db = pymysql.connect(host='134.175.198.59', user='zkhb', password='zkhb', port=3306, db='ehdata')
 cursor = db.cursor()
 user = 'tgtg'
 sql = 'INSERT INTO inori(name) values(%s)'
